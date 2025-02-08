5d8e3ca5c44c24fd9dabb3f1e7781af4249a3d5d
dante <dante.tarragau@gmail.com>
2025-02-07 21:53:58 -0500

    refactor: eliminar botón de agregar nuevo usuario en la gestión de empleados

--- src/app/(dashboard)/admin/components/EmployeeManagement.tsx
+++ src/app/(dashboard)/admin/components/EmployeeManagement.tsx
@@ -40,12 +40,6 @@ const EmployeeManagement = ({ columns, getAllEmployees }: Props) => {
     <>
       <div className='flex justify-between items-center mb-4'>
         <h2 className='font-bold text-lg uppercase'>Usuarios</h2>
-        {/* <button
-          className='bg-gray-300 text-white py-2 px-4 rounded hover:bg-gray-400'
-          onClick={() => handleOpenModal('create')}
-        >
-          Agregar nuevo usuario
-        </button> */}
         <button
           className={`px-4 py-2 flex items-center justify-center rounded-full bg-backbuttondefault`}
           onClick={() => handleOpenModal('create')}
